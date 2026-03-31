ed5cdb4295b1437710216a6e6470d819c1a92f4a
shahzaibriaz69 <shahzaibraiz0023@gmail.com>
2026-03-31 18:51:19 +0500

    /business-homepage-groomer-space-Owner page (To be continued...)

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/cookies-overlay-card', function () {
     return view('components.ui.cookies-overlay-card');
 })->name('cookies-overlay-card');
 
+Route::get('/business-homepage-groomer-space-Owner', function () {
+    return view('business-homepage-groomer-space-Owner');
+})->name('business-homepage-groomer-space-Owner');
+
 
 
 Route::view('dashboard', 'dashboard')
