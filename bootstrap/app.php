2310565039d711704cb53a66bafbf5eef8010926
Muhammad Wahab Sharif <www.rex19@gmail.com>
2026-04-06 18:42:37 +0500

    undo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-$app = new Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
-
-return $app;
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->redirectUsersTo('/');
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
