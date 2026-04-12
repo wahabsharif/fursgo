8e7c942a10e551a0c44017e29ae9206d69a3f6cd
Muhammad Wahab Sharif <www.rex19@gmail.com>
2026-04-12 22:37:55 +0500

    force Root Url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $root = config('app.url');
+        if (is_string($root) && $root !== '') {
+            URL::forceRootUrl(rtrim($root, '/'));
+        }
     }
 }
