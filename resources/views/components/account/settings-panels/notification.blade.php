81c0670501a614b961bd299bb32994ae65cb1a2a
Muhammad Wahab Sharif <www.rex19@gmail.com>
2026-07-22 19:33:16 +0500

    Notifications

---
+++ resources/views/components/account/settings-panels/notification.blade.php
@@ -0,0 +1,13 @@
+<h1 class="large-font">Notifications</h1>
+
+<x-account.settings-toggle setting="notify_booking_updates" label="Booking Updates"
+    description="Notify me when a booking is confirmed or changed." />
+
+<x-account.settings-toggle setting="notify_groomer_messages" label="Client Messages Zone"
+    description="Get alerts when clients send you a message." />
+
+<x-account.settings-toggle setting="notify_promotions" label="Promotions & Offers"
+    description="Receive special deals and exclusive discounts." />
+
+<x-account.settings-toggle setting="notify_reminder_alerts" label="Reminder Alerts"
+    description="Send reminders 24 hours before my booking." />
