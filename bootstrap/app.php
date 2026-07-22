a0c5903ec842a298c77d09ea3eb2290a5c2cfe4d
Muhammad Wahab Sharif <www.rex19@gmail.com>
2026-07-22 19:30:25 +0500

    session middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,20 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Application;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectUsersTo('/');
+        $middleware->web(append: [
+            \App\Http\Middleware\TrackAccountLoginSession::class,
+        ]);
         $middleware->alias([
             'auth.groomer_spacer' => \App\Http\Middleware\EnsureGroomerSpacerAuthenticated::class,
             'business.shell.web' => \App\Http\Middleware\SetBusinessPageWebShell::class,
@@ -30,4 +33,5 @@ return Application::configure(basePath: dirname(__DIR__))
                 return redirect($referer ?: '/');
             }
         });
-    })->create();
+    })
+    ->create();
