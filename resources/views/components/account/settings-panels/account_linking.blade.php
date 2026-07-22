c11c20e45b8d150bfcfef255952aed6c94882974
Muhammad Wahab Sharif <www.rex19@gmail.com>
2026-07-22 19:32:21 +0500

    Account Linking component

---
+++ resources/views/components/account/settings-panels/account_linking.blade.php
@@ -0,0 +1,92 @@
+<h1 class="large-font">Account Linking</h1>
+
+<div class="settings-section-content d-flex flex-column justify-content-between mt-5 mb-5 gap-25">
+    <p class="bold-font">Connected Accounts</p>
+
+    <div class="d-flex align-items-center justify-content-between gap-25">
+        <p style="color: #9D9B98">Link/unlink Google, Apple, or Facebook for login convenience</p>
+    </div>
+</div>
+
+<div class="account-linking-card d-flex align-items-center justify-content-between mt-4">
+    <div class="image-text d-flex align-items-center gap-10">
+        <div class="border-and-bg" style="border: none;">
+            <img src="{{ asset('images/social_media/facebook.png') }}" class="social-icons" alt="">
+
+        </div>
+        <div>
+            <p style="color:#9D9B98">Facebook Connected</p>
+        </div>
+    </div>
+    <div>
+        <a href="" class="small-link-tag">Unlink</a>
+    </div>
+</div>
+
+<div class="account-linking-card d-flex align-items-center justify-content-between mt-4">
+    <div class="image-text d-flex align-items-center gap-10">
+        <div class="border-and-bg">
+            <img src="{{ asset('images/social_media/google.png') }}" class="social-icons" alt="">
+
+        </div>
+        <div>
+            <p>Connect your Google Account</p>
+        </div>
+    </div>
+    <div>
+        <a href="" class="small-link-tag">Unlink</a>
+    </div>
+</div>
+
+<hr class="mt-4" style="border-top: 1px solid #E2E2E2;">
+
+<div class="account-linking-card d-flex align-items-center justify-content-between mt-4">
+    <div class="image-text d-flex align-items-center gap-10">
+        <div class="border-and-bg">
+            <img src="{{ asset('images/social_media/linkedin.png') }}" class="social-icons" alt="">
+
+        </div>
+        <div>
+            <p>Connect your LinkedIn Account</p>
+        </div>
+    </div>
+    <div>
+        <a href="" class="small-link-tag">Unlink</a>
+    </div>
+</div>
+
+<hr class="mt-4" style="border-top: 1px solid #E2E2E2;">
+
+<div class="account-linking-card d-flex align-items-center justify-content-between mt-4">
+    <div class="image-text d-flex align-items-center gap-10">
+        <div class="border-and-bg">
+            <img src="{{ asset('images/social_media/twitter.png') }}" class="social-icons" alt="">
+
+        </div>
+        <div>
+            <p>Connect your X Account</p>
+        </div>
+    </div>
+    <div>
+        <a href="" class="small-link-tag">Unlink</a>
+    </div>
+</div>
+
+<hr class="mt-4" style="border-top: 1px solid #E2E2E2;">
+
+<div class="account-linking-card d-flex align-items-center justify-content-between mt-4">
+    <div class="image-text d-flex align-items-center gap-10">
+        <div class="border-and-bg">
+            <img src="{{ asset('images/social_media/apple.png') }}" class="social-icons" alt="">
+
+        </div>
+        <div>
+            <p>Connect your Apple Account</p>
+        </div>
+    </div>
+    <div>
+        <a href="" class="small-link-tag">Unlink</a>
+    </div>
+</div>
+
+<hr class="mt-4" style="border-top: 1px solid #E2E2E2;">
