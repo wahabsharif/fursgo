7471a15accb733e034ce261ed7f0c58fff9156ef
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2026-09-24 12:44:14 +0500

    Admin Ui set as said in amendments notes

--- resources/views/admin/tabs/pet-owners.blade.php
+++ resources/views/admin/tabs/pet-owners.blade.php
@@ -71,6 +71,11 @@ $ticketStatusLabels = [
     disputeSearch: '',
     ticketFilter: 'all',
     ticketSearch: '',
+
+    // Mass-select for export
+    selectedIds: [],
+    allIds: @js(collect($customers)->pluck('id')->values()),
+
     openCustomer(id) {
         this.selectedCustomerId = id;
         this.detailTab = 'overview';
@@ -83,6 +88,23 @@ $ticketStatusLabels = [
         this.selectedCustomerId = null;
         this.detailTab = 'overview';
         this.selectedPetId = null;
+    },
+
+    isSelected(id) {
+        return this.selectedIds.includes(id);
+    },
+    toggleOne(id) {
+        if (this.isSelected(id)) {
+            this.selectedIds = this.selectedIds.filter(x => x !== id);
+        } else {
+            this.selectedIds.push(id);
+        }
+    },
+    get allSelected() {
+        return this.selectedIds.length === this.allIds.length;
+    },
+    toggleAll() {
+        this.selectedIds = this.allSelected ? [] : [...this.allIds];
     },
 }"
 @admin-pet-selected.window="selectedPetId = $event.detail.id">
@@ -274,7 +296,12 @@ $ticketStatusLabels = [
                         <thead>
                             <tr>
                                 <th class="admin-po-check-col">
-                                    <input type="checkbox" class="admin-po-check" aria-label="Select all customers">
+                                    <input type="checkbox"
+                                        class="admin-po-check"
+                                        aria-label="Select all customers"
+                                        :checked="allSelected"
+                                        @change="toggleAll()"
+                                        @click.stop>
                                 </th>
                                 <th>Name · User ID <span class="admin-sort" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" width="7" height="5" viewBox="0 0 7 5" fill="none">
                                             <path d="M6.15674 0.5L3.32831 3.32843L0.499884 0.5" stroke="#9C9A97" stroke-linecap="round" />
@@ -323,7 +350,11 @@ $ticketStatusLabels = [
                                      '{{ strtolower($customer['email']) }}'.includes(search.toLowerCase()) ||
                                      '{{ strtolower($customer['id']) }}'.includes(search.toLowerCase()))">
                                 <td class="admin-po-check-col" @click.stop>
-                                    <input type="checkbox" class="admin-po-check" aria-label="Select {{ $customer['name'] }}">
+                                    <input type="checkbox"
+                                        class="admin-po-check"
+                                        aria-label="Select {{ $customer['name'] }}"
+                                        :checked="isSelected('{{ $customer['id'] }}')"
+                                        @change="toggleOne('{{ $customer['id'] }}')">
                                 </td>
                                 <td>
                                     <span class="admin-po-name-cell">
